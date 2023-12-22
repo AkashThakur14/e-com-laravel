c57d9c1d043961b111f815e0ca8c08be06088c75
Akash <ak9988385532@gmail.com>
2023-12-22 15:58:07 +0530

    changes cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -49,9 +49,8 @@ class ProductController extends Controller
     }
 
 
-    static function cartItem()
-    {
-        $userId= Session::get('user')['id'];
-        return Cart::where('user_id',$userId)->count();
-    }
+ static function cartItem(){
+    $userId = Session::get('user',['id']);
+    return Cart::where('user_id',$userId)->count();
+}
 }
